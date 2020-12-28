f1540e966bdb4fab39d9571d340df1959fffa720
Joseph Bateh <github@josephbateh.com>
2020-12-28 10:55:52 -0500

    20201228

--- Operating Systems/Windows/windows_bloat.ps1
+++ Operating Systems/Windows/windows_bloat.ps1
@@ -1,6 +1,5 @@
 Get-AppxPackage *3dbuilder* | Remove-AppxPackage
 Get-AppxPackage *windowsalarms* | Remove-AppxPackage
-Get-AppxPackage *windowscalculator* | Remove-AppxPackage
 Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
 Get-AppxPackage *windowscamera* | Remove-AppxPackage
 Get-AppxPackage *officehub* | Remove-AppxPackage
@@ -15,9 +14,6 @@ Get-AppxPackage *bingnews* | Remove-AppxPackage
 Get-AppxPackage *onenote* | Remove-AppxPackage
 Get-AppxPackage *people* | Remove-AppxPackage
 Get-AppxPackage *windowsphone* | Remove-AppxPackage
-Get-AppxPackage *photos* | Remove-AppxPackage
-Get-AppxPackage *windowsstore* | Remove-AppxPackage
 Get-AppxPackage *bingsports* | Remove-AppxPackage
 Get-AppxPackage *soundrecorder* | Remove-AppxPackage
 Get-AppxPackage *bingweather* | Remove-AppxPackage
-Get-AppxPackage *xboxapp* | Remove-AppxPackage
