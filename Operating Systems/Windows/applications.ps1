719e9b083427d891e949fec1c699300f1b41bda4
Joseph Bateh <github@josephbateh.com>
2020-12-28 20:39:32 -0500

    20201228

---
+++ Operating Systems/Windows/applications.ps1
@@ -0,0 +1,19 @@
+choco feature enable -n=allowGlobalConfirmation
+
+choco install 7zip
+choco install adobereader
+choco install curl
+choco install discord
+choco install etcher
+choco install firefox
+choco install git
+choco install goodsync
+choco install googlechrome
+choco install leagueoflegends
+choco install rufus
+choco install steam
+choco install ueli
+choco install visualstudiocode
+choco install vlc
+choco install windirstat
+choco install zoom
