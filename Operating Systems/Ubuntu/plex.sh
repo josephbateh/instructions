4898df5d5dfd2afb2add7f188823d2ae8011a535
Joseph Bateh <github@josephbateh.com>
2021-05-06 05:17:36 -0400

    2021-05-06

--- Operating Systems/Ubuntu/plex.sh
+++ Operating Systems/Ubuntu/plex.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -ex
 
-# Install TV Tuner Packages
-sudo add-apt-repository ppa:b-rad/kernel+mediatree+hauppauge
-sudo apt-get update
-sudo apt-get install linux-mediatree
-sudo apt-get install linux-firmware-hauppauge
-
 # Install Plex
 echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
