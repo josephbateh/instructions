6097a2fd0fb6d62638094ebbe890d5c840d8c29f
Joseph Bateh <github@josephbateh.com>
2021-06-05 14:20:44 -0400

    Revert "2021-06-05"
    This reverts commit a0adc68e3ed77c2c0c944294c8d3a73d6c47627d.

--- Duplicity/restore.sh
+++ Duplicity/restore.sh
@@ -1,2 +1,3 @@
 #!/bin/bash -ex
+source duplicity.env
 env PASSPHRASE=${PASSPHRASE} duplicity restore --progress b2://${BB_KEY_ID}:${BB_APPLICATION_KEY}@${BB_BUCKET_ID}/${BB_BUCKET_PATH} ${LOCAL_PATH}
