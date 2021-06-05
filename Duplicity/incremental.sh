a0adc68e3ed77c2c0c944294c8d3a73d6c47627d
Joseph Bateh <github@josephbateh.com>
2021-06-05 14:15:10 -0400

    2021-06-05

--- Duplicity/incremental.sh
+++ Duplicity/incremental.sh
@@ -1,3 +1,2 @@
 #!/bin/bash -ex
-source duplicity.env
 env PASSPHRASE=${PASSPHRASE} duplicity incremental --progress ${LOCAL_PATH} b2://${BB_KEY_ID}:${BB_APPLICATION_KEY}@${BB_BUCKET_ID}/${BB_BUCKET_PATH}
--- Duplicity/list.sh
+++ Duplicity/list.sh
@@ -1,3 +1,2 @@
 #!/bin/bash -ex
-source duplicity.env
 env PASSPHRASE=${PASSPHRASE} duplicity list-current-files b2://${BB_KEY_ID}:${BB_APPLICATION_KEY}@${BB_BUCKET_ID}/${BB_BUCKET_PATH}
