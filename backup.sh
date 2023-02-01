e1b2ba362ec1d0ae1ee962ef48b897a8bc4ded0b
Joseph Bateh <github@josephbateh.com>
2023-01-31 19:26:53 -0500

    2023-01-31

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /Users/joseph/code/instructions
+cd /Users/joseph/code/instructions || exit
 date=$(date +%Y-%m-%d)
 git checkout main
 git add .
-git commit -m $date
+git commit -m "${date}"
 git push
