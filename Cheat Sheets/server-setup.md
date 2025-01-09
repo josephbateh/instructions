730bbfddf86bbb4bddecd0085d4e2b2a0e7047ab
Joseph Bateh <github@josephbateh.com>
2025-01-09 16:27:29 -0500

    Updates

---
+++ Cheat Sheets/server-setup.md
@@ -0,0 +1,12 @@
+# Server Setup
+
+## Checklist
+
+- [ ] Install [dotfiles](https://github.com/josephbateh/dotfiles).
+- [ ] Install [JoeBot](https://github.com/josephbateh/joebot).
+- [ ] Install Execute `setup.sh` [script](https://github.com/josephbateh/instructions/blob/main/Docker/setup/setup.sh).
+- [ ] Compile `docker-compose.yml` file from the Docker [directory](https://github.com/josephbateh/instructions/blob/main/Docker).
+- [ ] Add `WEBPASSWORD` for PiHole.
+- [ ] Add `ACCOUNT_TOKEN` for Folding@Home.
+- [ ] Install `docker-compose.yml` file.
+- [ ] Install [crontab](https://github.com/josephbateh/instructions) file.
--- Docker/setup/setup.sh
+++ Docker/setup/setup.sh
@@ -23,8 +23,21 @@ sudo apt install -y fail2ban
 sudo ufw allow ssh
 yes | sudo ufw enable
 
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
 # Install Docker
-curl -sSL https://get.docker.com/ | sh
-sudo usermod -aG docker joseph
-sudo apt install -y docker-compose
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo dockerd
+sudo usermod -aG docker joseph
